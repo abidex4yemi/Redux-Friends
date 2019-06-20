7d392f849aee7fd149b3610e121c998975466ec3
abidex4yemi <abidex4yemi@gmail.com>
2019-06-20 23:18:57 +0100

    Add styles to login form

--- friends/src/views/Login/Login.jsx
+++ friends/src/views/Login/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { login } from '../../redux/actions';
 import { Input, Button } from '../../components/Form';
@@ -42,27 +43,47 @@ const Login = props => {
 	};
 
 	return (
-		<form>
-			<Input
-				value={form.username}
-				name="username"
-				placeholder="Enter username..."
-				inputChange={inputChange}
-				type="text"
-				error={form.errors.username}
-			/>
+		<main>
+			<StyledContainer>
+				<StyledForm>
+					<h2>login</h2>
+					<form>
+						<Input
+							value={form.username}
+							name="username"
+							placeholder="Enter username..."
+							inputChange={inputChange}
+							type="text"
+							error={form.errors.username}
+						/>
 
-			<Input
-				value={form.password}
-				name="password"
-				placeholder="Enter password..."
-				inputChange={inputChange}
-				type="password"
-				error={form.errors.password}
-			/>
-			<Button buttonText="login" onclick={handleLogin} type="button" />
-		</form>
+						<Input
+							value={form.password}
+							name="password"
+							placeholder="Enter password..."
+							inputChange={inputChange}
+							type="password"
+							error={form.errors.password}
+						/>
+						<Button buttonText="login" onclick={handleLogin} type="button" />
+					</form>
+				</StyledForm>
+			</StyledContainer>
+		</main>
 	);
 };
 
 export default connect(null, { login })(Login);
+
+const StyledContainer = styled.div`
+	width: 80%;
+	margin: 0 auto;
+	display: flex;
+	justify-content: center;
+`;
+
+const StyledForm = styled.div`
+	width: 350px;
+	border: 1px solid red;
+	padding: 2rem;
+`;
