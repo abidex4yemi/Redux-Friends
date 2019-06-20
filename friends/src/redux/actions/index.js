a59d1b63d36fe65ec8c5c70020c56496fbbf5505
abidex4yemi <abidex4yemi@gmail.com>
2019-06-20 12:46:24 +0100

    Create friendsReducer

--- friends/src/redux/actions/index.js
+++ friends/src/redux/actions/index.js
@@ -1,7 +1,6 @@
 // Action Types
-export const FETCHING = 'FETCHING';
-export const FAILURE = 'FAILURE';
-export const SUCCESS = 'SUCCESS';
+export const SET_ACTION_STATUS = 'FAILURE';
 export const DELETE_FRIEND = 'DELETE_FRIEND';
+export const ADD_FRIENDS = 'ADD_FRIENDS';
 export const ADD_FRIEND = 'ADD_FRIEND';
 export const UPDATE_FRIEND_INFO = 'UPDATE_FRIEND_INFO';
---
+++ friends/src/redux/reducers/friendsReducer.js
@@ -0,0 +1,44 @@
+import { ADD_FRIENDS, SET_ACTION_STATUS, DELETE_FRIEND, ADD_FRIEND, UPDATE_FRIEND_INFO } from '../actions';
+
+const InitialState = {
+	deletingFriend: false,
+	fetchingFriends: false,
+	friends: [],
+	loggingIn: false,
+	savingFriend: false,
+	updatingFriend: false,
+	removingFriend: false,
+	error: null
+};
+
+export const friendsReducer = (state = InitialState, action) => {
+	switch (action.type) {
+		case SET_ACTION_STATUS:
+			return {
+				...state,
+				...action.payload.status
+			};
+		case ADD_FRIENDS:
+			return {
+				...state,
+				friends: action.payload
+			};
+		case ADD_FRIEND:
+			return {
+				...state,
+				friends: [...state.friends, action.payload]
+			};
+		case DELETE_FRIEND:
+			return {
+				...state,
+				friends: action.payload
+			};
+		case UPDATE_FRIEND_INFO:
+			return {
+				...state,
+				friends: action.payload
+			};
+		default:
+			break;
+	}
+};
